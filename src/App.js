8f156ee0cb2231d604ae49c3570d5b5d07130c3d
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2020-02-09 14:29:55 +0100

    Creating basic layout for App.js & navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux'
+import { fetchingBills, fetchingCategories } from './store/actions/actions';
 
-class App extends Component {
-  render() {
-    return (
-      <div data-testid="App">
+import Tab from './components/Tab'
 
+const App = props => {
+  const [openTab, setOpenTab] = useState("bills")
+
+  const dispatch = useDispatch()
+
+  // On first render, retrieve bills and categories
+  useEffect(() => {
+    dispatch(fetchingBills())
+    dispatch(fetchingCategories())
+  }, [])
+
+  return (
+    <div data-testid="App">
+      <div className="switchBetweenTabs">
+        <div 
+          active={openTab === "bills"} 
+          onClick={() => setOpenTab("bills")}
+        >
+          Bills
+        </div>
+        <div 
+          active={openTab === "expenses"}
+          onClick={() => setOpenTab("expenses")}
+        >
+          Expenses
+        </div>
       </div>
-    );
-  }
+      {openTab}
+    </div>
+  );
 }
+
 
 export default App;
